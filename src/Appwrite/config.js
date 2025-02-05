7e699a0bd879b974a5f7f4283fb18d465c0c4f4b
Adil-Shahzad17 <adil1712shahzad@gmail.com>
2025-02-05 14:04:13 +0500

    Fix API Error

--- src/Appwrite/config.js
+++ src/Appwrite/config.js
@@ -1,4 +1,5 @@
 const config = {
+  pexels_api_key: String(import.meta.env.VITE_PEXELS_API_KEY),
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   EMAIL_JS_SERVICE_ID: String(import.meta.env.VITE_EMAILJS_SERVICE_ID),
--- src/Pexels/Pexels.js
+++ src/Pexels/Pexels.js
@@ -1,17 +1,12 @@
-import { createClient } from "pexels";
+import config from "../Appwrite/config";
 
-const PEXELS_API_KEY =
-  "ZppFcwbX01jtHKdt5HHWz54Xfdo02pkKp1ajmVEZKaOVPsSKN7uPOF3f";
+const PEXELS_API_KEY = config.pexels_api_key;
 
 class PEXELS {
-  constructor() {
-    this.client = createClient(PEXELS_API_KEY);
-  }
-
   async search({ query, per_page = 15, page = 1 }) {
     try {
       const response = await fetch(
-        `https://api.pexels.com/v1/search?query=${query}&per_page=${per_page}$page=${page}`,
+        `https://api.pexels.com/v1/search?query=${query}&per_page=${per_page}&page=${page}`,
         {
           headers: {
             Authorization: PEXELS_API_KEY,
@@ -19,29 +14,31 @@ class PEXELS {
         }
       );
 
-      if (response) {
-        const data = response.json();
+      if (response.ok) {
+        const data = await response.json();
         return data;
       }
     } catch (error) {
       console.log("Search Error", error);
+      return null;
     }
   }
 
   async nextPage({ nextURL }) {
     try {
-      const response = await fetch(`${nextURL}`, {
+      const response = await fetch(nextURL, {
         headers: {
           Authorization: PEXELS_API_KEY,
         },
       });
 
-      if (response) {
-        const data = response.json();
+      if (response.ok) {
+        const data = await response.json();
         return data;
       }
     } catch (error) {
       console.log("Search Error", error);
+      return null;
     }
   }
 }
